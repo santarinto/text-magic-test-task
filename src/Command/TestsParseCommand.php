f14d46c970d6928d0cf68383b1ce8dffb649c000
Glazkov V Ilya <santarinto@yandex.ru>
2024-04-27 12:55:39 +0800

    complete parse command

--- src/Command/TestsParseCommand.php
+++ src/Command/TestsParseCommand.php
@@ -2,6 +2,7 @@
 
 namespace App\Command;
 
+use App\Service\LinesParserService;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -14,7 +15,9 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 )]
 class TestsParseCommand extends Command
 {
-    public function __construct() {
+    public function __construct(
+        private readonly LinesParserService $linesParserService
+    ) {
         parent::__construct();
     }
 
@@ -23,7 +26,27 @@ class TestsParseCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output): int {
         $io = new SymfonyStyle($input, $output);
 
+        $content = file_get_contents(__DIR__ . '/../../data/tests.txt');
+        $blocks = explode("\n\n\n", $content);
+        $countBlocks = count($blocks);
 
+        for ($i = 0; $i < $countBlocks; $i++) {
+            $taskNumber = $i + 1;
+            $io->writeln("\nBlock $taskNumber:");
+
+            $dto = $this->linesParserService->parse(explode("\n", $blocks[$i]));
+
+            $io->writeln("\t\tExpression: {$dto->getExpression()}");
+
+            $countResults = count($dto->getResults());
+            for ($j = 0; $j < $countResults; $j++) {
+                $resultNumber = $j + 1;
+
+                $io->writeln("\t\tresult[{$resultNumber}]: {$dto->getResults()[$j]}");
+            }
+
+            $io->writeln("\t\tCompiled answer: {$dto->getCompiledAnswer()}");
+        }
 
         $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
 
