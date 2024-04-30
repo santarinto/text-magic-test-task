dc8ad116ab4a38bf5b1de0e43151f5b3807c153a
Glazkov V Ilya <santarinto@yandex.ru>
2024-04-30 10:23:13 +0800

    prepare Commands to work with DB

--- src/Command/TestsRewindCommand.php
+++ src/Command/TestsRewindCommand.php
@@ -16,33 +16,16 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 )]
 class TestsRewindCommand extends Command
 {
-    public function __construct()
-    {
+    public function __construct() {
         parent::__construct();
     }
 
-    protected function configure(): void
-    {
-        $this
-            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
-            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
-        ;
-    }
+    protected function configure(): void {}
 
-    protected function execute(InputInterface $input, OutputInterface $output): int
-    {
+    protected function execute(InputInterface $input, OutputInterface $output): int {
         $io = new SymfonyStyle($input, $output);
-        $arg1 = $input->getArgument('arg1');
 
-        if ($arg1) {
-            $io->note(sprintf('You passed an argument: %s', $arg1));
-        }
-
-        if ($input->getOption('option1')) {
-            // ...
-        }
-
-        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+        $io->success('Complete!');
 
         return Command::SUCCESS;
     }
